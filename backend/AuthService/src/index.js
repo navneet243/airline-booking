4abdf98c26ca364f9a7e3f9ef63f93803898700c
navneet243 <navneetnamdev3@gmail.com>
2024-12-24 11:02:04 +0530

    added users and roles through user_roles

--- backend/AuthService/src/index.js
+++ backend/AuthService/src/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const {PORT} = require('./config/serverConfig');
 const ApiRoutes = require('./routes/index');
+const db =require('./models/index');
+const {User, Role} = require('./models/index');
 
 const setupAndStartServer = async () => {
     const app= express();
@@ -10,8 +12,21 @@ const setupAndStartServer = async () => {
 
     app.use('/api', ApiRoutes);
 
-    app.listen(PORT,()=>{
+    app.listen(PORT, async()=>{
         console.log(`Server listening at ${PORT}`);
+        if(process.env.DB_SYNC){
+            db.sequelize.sync({alter:false})
+        }
+        const u1 = await User.findByPk(7);
+        const r1 = await Role.findByPk(1);
+        if (u1 && r1) {
+            u1.addRole(r1);
+            // const response = await u1.getRoles();
+            // console.log(response);
+        } else {
+            console.log('User or Role not found');
+        }
+
     })
 }
 
